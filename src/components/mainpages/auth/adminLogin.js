ea6ec905fb81953828c2e3715a5978a05f260b84
tamil-vishnu-24 <92633011+tamil-vishnu-24@users.noreply.github.com>
2022-12-14 20:51:35 +0530

    Delete src directory

--- src/components/mainpages/auth/adminLogin.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function adminLogin() {
-  return (
-    <div>Admin Login</div>
-  )
-}
--- src/components/mainpages/utils/Notfound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function notfound() {
-  return (
-    <div>404 || Not found</div>
-  )
-}
